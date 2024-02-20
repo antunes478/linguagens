8af7bb7b026e0e7b7c0d56f2b87ce7d9d99f838d
antunes478 <146825563+antunes478@users.noreply.github.com>
2024-02-20 11:51:04 +0000

    alteracao na interface

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+      // Opções de configuração aqui
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      arrows: true
+    });
+  });
+  
